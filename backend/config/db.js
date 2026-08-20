6deca40b52398ec1ed732d8bf8f8ff8f02dbdc33
harithrazinm@gmail.com <harithrazinmgmail.com@Lol-2.local>
2026-08-20 09:43:04 +0800

    Configure SSL connection for Aiven database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,20 +2,38 @@ require("dotenv").config();
 
 const mysql = require("mysql2/promise");
 
+const ssl =
+    process.env.DB_SSL === "TRUE"
+        ? {
+            rejectUnauthorized: true,
+
+            ca: process.env.DB_SSL_CA
+                ? process.env.DB_SSL_CA.replace(/\\n/g, "\n")
+                : undefined
+        }
+        : undefined;
+
+
 const pool = mysql.createPool({
+
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
+
+    port: Number(process.env.DB_PORT),
+
     user: process.env.DB_USER,
+
     password: process.env.DB_PASSWORD,
+
     database: process.env.DB_NAME,
 
-    ssl: {
-        rejectUnauthorized: false
-    },
+    ssl,
 
     waitForConnections: true,
+
     connectionLimit: 10,
+
     queueLimit: 0
 });
+
 
 module.exports = pool;
